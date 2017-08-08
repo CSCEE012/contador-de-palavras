b3e7c7801c12dbdfa538613fe4da79c206741a17
CSCEE012 <caioshibuya@gmail.com>
2017-08-07 22:14:38 -0300

    Arquivo final, com código funcional. Obs:Palavras mistas de letras e números são consideradas como palavras diferentes. Ex: AA123 é considerado como duas palavras.

--- src/main.c
+++ src/main.c
@@ -11,12 +11,84 @@ int main() {
 
   char c;
 
+  int n=0,l=0, f=0;//n conta o # de palavras, l verifica se c é uma letra(1), numero(2), nenhum(0), f se é ponto ou virgula de float
+
   c = 1;
+  
 
   while (c != '\n') {
+
     scanf("%c", &c);
+
+    //Verifica se a letra (lida no momento) é o inicio de uma palavra, caso positivo incrementa o contador
+    if(((c >= 65)&&(c <= 90)) || ((c >= 97)&&(c <= 122))){
+      if(l==1){
+
+        l=1;
+        f=0;
+
+      }
+      else {
+
+        l=1;
+        f=0;
+        n++;
+    
+
+      }
+    }
+
+    //Verifica se o número (lido no momento) é o inicio de uma palvra, caso positivo incrmenta o contador
+    else if((c >= 48)&&(c <= 57)){
+      if(l==2){
+
+        l=2;
+        f=0;
+
+      }
+
+      else {
+        if(f==1){
+
+        l=2;
+
+        }
+
+        else{
+
+        n++;
+        l=2;
+
+        }
+      }
+    }
+
+    //Verifica se o '.' ou ',' são precedidos por um número, caso positivo incrementa a flag f
+    else if((c == 44) || (c == 46)){
+      if(l == 2){
+
+        f++;
+        l=0;
+
+      }
+
+      else{
+
+        f=0;
+        l=0;
+
+      }
+
+    }
+    else{
+
+        f=0;
+        l=0;
+ 
+    }
+
   }
 
-  printf("1\n");
+  printf("%d\n", n);
   return 0;
 }
